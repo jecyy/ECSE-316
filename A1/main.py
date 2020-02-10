6f83a9c9a04cfc445ac8bd87f778ce6a51a0b3f2
Bobqian <bobqian1007@outlook.com>
2020-02-10 17:50:26 -0500

    xia jb xie de1

--- A1/main.py
+++ A1/main.py
@@ -2,9 +2,26 @@ import argparse
 import re
 import socket
 class Packet(object):
-	def __init__(self,server,name):
-		self.server = server.split()
-
+	def __init__(self,name,type):
+		self.name = byte(self.formName(name.split('.')))
+		self.type = bytes([typeTransform(type)])
+	def formName(self,names):
+		formatName = []
+		for n in names:
+			formatName.append(len(n))
+			for c in n:
+				formatName.append(ord(c))
+		return formatName
+	def typeTransform(type):
+		typemap = {'A'：1,'NS':2,'MX':15}
+		return typemap[type]
+	def generateData(self):
+		
+class PacketParser(object):
+	def __init__(self,response):
+		self.ID
+		self.Type
+		self.TTL
 parser = argparse.ArgumentParser(description='DNS client')
 parser.add_argument('-t', type=int, default=5,help='time out of retransmit')
 parser.add_argument('-r', type=int, default=3,help='max-retries')
@@ -22,6 +39,15 @@ if args.mx and args.ns:
 if not re.match(r'^@\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$',args.server):
 	print('ERROR	invalid argument:format of server address is wrong')
 	exit()
-
-
+type = 'NS' if args.ns else 'MX' if args.mx else 'A'
+packet = Packet(args.name,type)
+print('DNS sending request for:',args.name)
+print('Server:',args.server[1:])
+print('Request type:',type)
+data = packet.generateData()
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(data,(args.server[1:],args.p)
+response,serverAddr = s.recvfrom(1024)
+if serverAddr != args.server[1:]:
+	print('ERROR	unexpect response:response from wrong server')
+	exit()
