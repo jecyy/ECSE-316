2dde37e95499254a481e6b4a57fd825c6bdc49bd
Bobqian <bobqian1007@outlook.com>
2020-02-11 11:25:40 -0500

    xia jb xie de4

--- A1/main.py
+++ A1/main.py
@@ -4,7 +4,7 @@ import socket
 class Packet(object):
 	def __init__(self,name,type):
 		self.name = bytes(self.formName(name.split('.')))
-		self.type = bytes([typeTransform(type)])
+		self.type = bytes([self.typeTransform(type)])
 	def formName(self,names):
 		formatName = []
 		for n in names:
@@ -12,16 +12,16 @@ class Packet(object):
 			for c in n:
 				formatName.append(ord(c))
 		return formatName
-	def typeTransform(type):
-		typemap = {'A'：1,'NS':2,'MX':15}
+	def typeTransform(self,type):
+		typemap = {'A':1,'NS':2,'MX':15}
 		return typemap[type]
 	def generateData(self):
-		
+		return b'\x82\x7a\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00'+self.name+b'\x00\x00'+self.type+b'\x00\x01'
 class PacketParser(object):
-	def __init__(self,response):
-		self.ID
-		self.Type
-		self.TTL
+	def __init__(self,response,data):
+		self.IDVerification = response[:2] == data[:2]
+		self.TTL = response[len(data)+8:len(data)+10]
+		self.autority = 'auth' if response[2] & 4 else 'non-auth'
 parser = argparse.ArgumentParser(description='DNS client')
 parser.add_argument('-t', type=int, default=5,help='time out of retransmit')
 parser.add_argument('-r', type=int, default=3,help='max-retries')
@@ -45,9 +45,11 @@ print('DNS sending request for:',args.name)
 print('Server:',args.server[1:])
 print('Request type:',type)
 data = packet.generateData()
+print(data)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(data,(args.server[1:],args.p)
+s.sendto(data,(args.server[1:],args.p))
 response,serverAddr = s.recvfrom(1024)
-if serverAddr != args.server[1:]:
+print(response)
+if serverAddr != (args.server[1:],args.p):
 	print('ERROR	unexpect response:response from wrong server')
 	exit()
